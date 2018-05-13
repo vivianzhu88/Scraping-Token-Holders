5f653f0f8f09af32022101a3be8b0006f4b1a7b2
vivianzhu88 <31047942+vivianzhu88@users.noreply.github.com>
2018-05-13 16:02:33 -0700

    Fix error in scraping loop

--- scrapeTokenHolders.py
+++ scrapeTokenHolders.py
@@ -12,49 +12,47 @@ def holders_data(url, iframe_id):
     time.sleep(5)
     iframe1 = driver.find_element_by_id(iframe_id)
     driver.switch_to.frame(iframe1)
-    soup = BeautifulSoup(driver.page_source, 'html.parser')
 
     #Clicking next button
-    token_holders = soup.find_all('tr')
-    del token_holders[0]
-
-    i=1
+    rank = []
+    address = []
+    quantity = []
+    percent = []
+    
+    i=0
+    token_holders=[]
     while True:
+        soup = BeautifulSoup(driver.page_source, 'html.parser')
+        token_holders = token_holders + soup.find_all('tr')
+        del token_holders[50*i]
+
+        for j in range(50*i, len(token_holders)):
+            h = token_holders[j]
+            holder_info = h.find_all('td')
+
+            rank.append(holder_info[0].get_text())
+            address.append(holder_info[1].get_text())
+            quantity.append(holder_info[2].get_text())
+            percent.append(holder_info[3].get_text())
+
+            #Putting data into a csv file
+            holders = pd.DataFrame({
+                'Rank': rank,
+                'Address': address,
+                'Quantity': quantity,
+                'Percentage': percent
+                })
+            holders = holders[['Rank','Address','Quantity','Percentage']]
+            holders.to_csv('holders.csv', index=False, encoding='utf-8')
+        
+        i+=1
         try:
             driver.find_element_by_link_text('Next').click()
         except NoSuchElementException:
             break
         time.sleep(5)
-        soup = BeautifulSoup(driver.page_source, 'html.parser')
-        token_holders = token_holders + soup.find_all('tr')
-        del token_holders[50*i]
-        i+=1
-        time.sleep(5)
 
-    #Putting all the scraped data from iframe into lists
-    rank = []
-    address = []
-    quantity = []
-    percent = []
 
-    for i in range(0, len(token_holders)):
-        h = token_holders[i]
-        holder_info = h.find_all('td')
-
-        rank.append(holder_info[0].get_text())
-        address.append(holder_info[1].get_text())
-        quantity.append(holder_info[2].get_text())
-        percent.append(holder_info[3].get_text())
-
-    #Putting data into a csv file
-    holders = pd.DataFrame({
-        'Rank': rank,
-        'Address': address,
-        'Quantity': quantity,
-        'Percentage': percent
-        })
-    holders = holders[['Rank','Address','Quantity','Percentage']]
-    holders.to_csv('holders.csv', index=False, encoding='utf-8')
 
     
 
